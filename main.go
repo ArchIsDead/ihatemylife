5436c92484e8bfbb9a73de7ee468fe257a47311b
ArchIsDead <ccryptohigh@gmail.com>
2026-09-06 12:49:17 +0700

    Update main.go

--- main.go
+++ main.go
@@ -49,40 +49,56 @@ func main() {
 		switch x {
 		case "1":
 			clear()
+			utils.ShowBanner(banner)
 			commands.N(r)
 		case "2":
 			clear()
+			utils.ShowBanner(banner)
 			commands.D(r)
 		case "3":
 			clear()
+			utils.ShowBanner(banner)
 			commands.U(r)
 		case "4":
 			clear()
+			utils.ShowBanner(banner)
 			commands.CS(r)
 		case "5":
 			clear()
+			utils.ShowBanner(banner)
 			commands.IS(r)
 		case "6":
 			clear()
+			utils.ShowBanner(banner)
 			commands.IP(r)
 		case "7":
 			clear()
+			utils.ShowBanner(banner)
 			commands.CIP()
 		case "8":
 			clear()
+			utils.ShowBanner(banner)
 			commands.KP(r)
 		case "9":
 			clear()
+			utils.ShowBanner(banner)
 			commands.NP(r)
 		case "10":
 			clear()
+			utils.ShowBanner(banner)
 			commands.WZ(r)
 		case "11":
 			clear()
+			utils.ShowBanner(banner)
 			commands.TM(r)
 		case "12":
 			clear()
-			commands.DR(r)
+			utils.ShowBanner(banner)
+			commands.NS(r)
+		case "13":
+			clear()
+			utils.ShowBanner(banner)
+			commands.GS(r)
 		case "0", "exit", "quit":
 			clear()
 			utils.Err("Exit.")
