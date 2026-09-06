a4af583c1a00f5330994d7f3a179b1afd7a5f5ff
ArchIsDead <ccryptohigh@gmail.com>
2026-09-06 19:55:23 +0700

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -33,6 +33,7 @@ func Menu() {
 	fmt.Println(Gry("[17]") + Wht(" Encoder"))
 	fmt.Println(Gry("[18]") + Wht(" Decoder"))
 	fmt.Println(Gry("[19]") + Wht(" Presets"))
+	fmt.Println(Gry("[20]") + Wht(" AI Chat"))
 	fmt.Println(Gry("[00]") + Red(" Exit"))
 	fmt.Println(Div())
 }
