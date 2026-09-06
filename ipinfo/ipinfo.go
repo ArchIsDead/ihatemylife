15fe405e6270b2ab30a2d6fdbc7877c06e372033
ArchIsDead <ccryptohigh@gmail.com>
2026-09-06 14:59:01 +0700

    Implement IP lookup functionality in ipinfo.go

--- ipinfo/ipinfo.go
+++ ipinfo/ipinfo.go
@@ -1,3 +1,76 @@
+package ipinfo
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"s/utils"
+)
+
+type Result struct {
+	IP        string
+	Success   bool
+	Type      string
+	Continent string
+	Country   string
+	Region    string
+	City      string
+	Lat       string
+	Lon       string
+	Org       string
+	ISP       string
+	Timezone  string
+}
+
+func Lookup(ip string) (*Result, error) {
+	u := "https://ipwho.is/"
+	if ip != "" {
+		u += ip
+	}
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var raw map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&raw); err != nil {
+		return nil, err
+	}
+
+	r := &Result{
+		IP:      str(raw["ip"]),
+		Success: raw["success"] == true,
+		Type:    str(raw["type"]),
+	}
+
+	if c, ok := raw["continent"].(string); ok {
+		r.Continent = c
+	}
+	if c, ok := raw["country"].(string); ok {
+		r.Country = c
+	}
+	if c, ok := raw["region"].(string); ok {
+		r.Region = c
+	}
+	if c, ok := raw["city"].(string); ok {
+		r.City = c
+	}
+	r.Lat = fmt.Sprintf("%v", raw["latitude"])
+	r.Lon = fmt.Sprintf("%v", raw["longitude"])
+
+	if conn, ok := raw["connection"].(map[string]interface{}); ok {
+		r.ISP = str(conn["isp"])
+		r.Org = str(conn["org"])
+	}
+	if tz, ok := raw["timezone"].(map[string]interface{}); ok {
+		r.Timezone = str(tz["id"])
+	}
+
+	return r, nil
+}
+
 func (r *Result) Show() {
 	fmt.Println(utils.Div())
 	fmt.Println(utils.Bld(utils.Wht("[ IP INFO ]")))
@@ -42,4 +115,12 @@ func (r *Result) Show() {
 	fmt.Println()
 	fmt.Print(utils.Gry("Press Enter to return..."))
 	fmt.Scanln()
+}
+
+func str(x interface{}) string {
+	if x == nil {
+		return ""
+	}
+	s, _ := x.(string)
+	return s
 }
