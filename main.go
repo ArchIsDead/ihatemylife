93e37d1b166cb2eecae8bc2c69f8c8d8809e7062
ArchIsDead <ccryptohigh@gmail.com>
2026-09-06 17:58:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,17 +9,10 @@ import (
 	"strings"
 
 	"s/commands"
+	"s/music"
+	"s/preset"
 	"s/utils"
 )
-
-var banner = `
-  _________     .__       ____         .___      
- /   _____/__ __|__| ____/_   |      __| _/____  
- \_____  \|  |  \  |/ ___\|   |     / __ |/ __ \ 
- /        \  |  /  \  \___|   |    / /_/ \  ___/ 
-/_______  /____/|__|\___  >___| /\ \____ |\___  >
-        \/              \/      \/      \/    \/ 
-`
 
 func clear() {
 	if runtime.GOOS == "windows" {
@@ -33,9 +26,17 @@ func clear() {
 	}
 }
 
+func startMusic() {
+	if path, ok := preset.GetSelectedMusic(); ok && path != "" {
+		music.Play(path)
+	}
+}
+
 func showMain(r *bufio.Reader) {
 	clear()
+	banner := preset.GetSelectedBanner()
 	utils.ShowBanner(banner)
+
 	for {
 		utils.Menu()
 		fmt.Print(utils.Prompt("\n> "))
@@ -45,110 +46,143 @@ func showMain(r *bufio.Reader) {
 		if x == "" {
 			x = "0"
 		}
+
 		switch x {
 		case "1":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.N(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "2":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.D(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "3":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.U(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "4":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.CS(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "5":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.IS(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "6":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.IP(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "7":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.CIP()
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "8":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.KP(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "9":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.NP(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "10":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.WZ(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "11":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.TM(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "12":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.NS(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "13":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.GS(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "14":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.UN(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "15":
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			commands.SH(r)
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
+		case "16":
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+			commands.SF(r)
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+		case "17":
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+			commands.HA(r)
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+		case "18":
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+			commands.EN(r)
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+		case "19":
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+			commands.DE(r)
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+		case "20":
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
+			commands.PR(r)
+			clear()
+			utils.ShowBanner(preset.GetSelectedBanner())
 		case "0", "exit", "quit":
 			clear()
+			music.Stop()
 			utils.Err("Exit.")
 			os.Exit(0)
 		default:
 			clear()
-			utils.ShowBanner(banner)
+			utils.ShowBanner(preset.GetSelectedBanner())
 			utils.Err("Unknown command.")
 		}
 	}
 }
 
 func main() {
+	startMusic()
 	r := bufio.NewReader(os.Stdin)
 	showMain(r)
 }
