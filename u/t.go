70fb3e1a33ca553d235ab3714d3f88b0f42d030d
ArchIsDead <ccryptohigh@gmail.com>
2026-09-06 11:01:07 +0700

    Create t.go

---
+++ u/t.go
@@ -0,0 +1,12 @@
+package u
+
+func B(b string) string { return GW(b) }
+func T(t string) string { return B(W(t)) }
+func S(t string) string { return G(t) }
+func P(t string) string { return C(t) }
+func I(t string) string { return W(t) }
+func N(t string) string { return N(t) }
+func E(t string) string { return R(t) }
+func A(t string) string { return Y(t) }
+func J(t string) string { return BW(t) }
+func H() string          { return G("────────────────────────────────────────") }
