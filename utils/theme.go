496ed0ee72b5f8c10c0766cff5e30bf9e40624b9
ArchIsDead <ccryptohigh@gmail.com>
2026-09-06 11:11:22 +0700

    Update theme.go

--- utils/theme.go
+++ utils/theme.go
@@ -1,11 +1,13 @@
 package utils
 
-func B(b string) string { return GW(b) }
+import "fmt"
+
+func B(b string) { fmt.Println(GW(b)) }
 func T(t string) string { return B(W(t)) }
-func S(t string) string { return G(t) }
+func S(t string) { fmt.Println(G(t)) }
 func P(t string) string { return C(t) }
 func I(t string) string { return W(t) }
-func E(t string) string { return R(t) }
+func E(t string) { fmt.Println(R(t)) }
 func A(t string) string { return Y(t) }
 func J(t string) string { return BW(t) }
-func H() string          { return G("────────────────────────────────────────") }
+func H() string { return G("────────────────────────────────────────") }
